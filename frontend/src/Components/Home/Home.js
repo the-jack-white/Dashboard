11164cdc652056119587f370b0f84ba86e175790
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2019-12-03 11:19:01 +0200

    Update Home Component
    Add Bootstrap Buttons for Sign Up and Login

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Button } from 'react-bootstrap';
+
+import './Home.css';
 
 const Home = () => {
     return (
-        <h3>Home Page</h3>
+        <Jumbotron className="home-component">
+            <div className="button-container">
+                <Button className="button" variant="primary">Sign Up</Button>
+                <Button className="button" variant="outline-primary" as={Link} to='/dashboard'>Login</Button>
+            </div>
+        </Jumbotron>
     )
 }
 
